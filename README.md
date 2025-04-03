862de467278e5262c5fcc3e5fa213529df9a9bc1
Uped <56954025+upedd@users.noreply.github.com>
2025-04-03 18:48:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Spotify Currently Playing Display for 64x32 LED Matrix
+
+Display the song you are currently playing on Spotify on a 64x32 LED matrix using a CircuitPython-supported microcontroller.
+
+## Features
+- Displays the title and artist(s) of the song.
+- Shows scaled-down, colorful cover art.
+- Includes a progress bar for the currently playing song.
+- Supports on-device cover art fetching and decoding.
+- Features 5-bit color depth.
+- Operates on as little as 8 MB of RAM!
+
+## Installation
+1. Install CircuitPython on your board: [Guide](https://learn.adafruit.com/welcome-to-circuitpython/installing-circuitpython).
+2. Clone this repository.
+3. Copy `code.py` and the `fonts` folder to your device's root directory.
+4. Copy the contents of the `libs` folder to your device's `libs` directory.
+5. Create a new application on the Spotify Developer Dashboard: [Spotify Dashboard](https://developer.spotify.com/dashboard). Take note of the **Client ID** and **Client Secret**.
+6. Visit [Spotify Refresh Token Generator](https://spotify-refresh-token-generator.netlify.app) to generate a refresh token with the `user-read-currently-playing` scope.
+7. Open `settings.toml` in your device directory and paste in the following, filling in your details:
+    ```toml
+    CIRCUITPY_WIFI_SSID = "your Wi-Fi SSID"
+    CIRCUITPY_WIFI_PASSWORD = "your Wi-Fi password"
+    SPOTIFY_CLIENT_ID = "your Spotify Client ID"
+    SPOTIFY_CLIENT_SECRET = "your Spotify Client Secret"
+    SPOTIFY_REFRESH_TOKEN = "your generated refresh token"
+    ```
+8. Enjoy your Spotify Matrix Display!
+
+## Modifying for Other Boards
+This code has been tested with Hack Club's Neon but should work with any CircuitPython-compatible microcontroller connected to a 64x32 LED matrix. You might need to modify the `RGBMatrix` constructor call on line 138.
---
+++ code.py
@@ -0,0 +1,276 @@
+# MIT License
+
+# Copyright (c) 2025 uped
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import adafruit_display_text.label
+import board
+import displayio
+import framebufferio
+import rgbmatrix
+import terminalio
+from jpegio import JpegDecoder
+from adafruit_bitmap_font import bitmap_font
+import gc
+import os
+import adafruit_connection_manager
+import wifi
+import adafruit_requests
+import time
+import binascii
+
+def base64_encode(input_string):
+    encoded_bytes = binascii.b2a_base64(input_string.encode("utf-8")).strip()
+    return encoded_bytes.decode("utf-8")
+
+class CurrentlyPlayingInfo:
+    def __init__(self, name, artists, cover_url, id, progress, duration):
+        self.name = name
+        self.artists = artists
+        self.cover_url = cover_url
+        self.id = id
+        self.progress = progress
+        self.duration = duration
+
+class SpotifyClient:
+    def __init__(self, client_id, client_secret, refresh_token, err_handler):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.refresh_token = refresh_token
+        self.err_handler = err_handler
+
+    def fetch_access_token(self):
+        url = "https://accounts.spotify.com/api/token"
+        payload = {
+            'grant_type': 'refresh_token',
+            'refresh_token': spotify_refresh_token
+        }
+        client = spotify_client_id + ':' + spotify_client_secret
+        
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + base64_encode(client)
+        }
+
+        response = requests.post(url, data=payload, headers=headers)
+        if response.status_code != 200:
+            self.err_handler(f'Failed to fetch access token from spotify. Code returned: {response.status_code}')
+            return False
+
+        response_data = response.json()
+
+        self.access_token = response_data.get('access_token')
+        if 'refresh_token' in response_data:
+            self.refresh_token = response_data['refresh_token']
+        return True
+
+    def get_currently_playing(self, is_retry=False):
+        if not hasattr(self, 'access_token'):
+            if not self.fetch_access_token():
+                return None
+
+        url = "https://api.spotify.com/v1/me/player/currently-playing"
+        headers = {
+            'Authorization': 'Bearer ' + self.access_token
+        }
+        response = requests.get(url, headers=headers)
+        if response.status_code == 204:
+            return None
+        elif response.status_code == 401 and not is_retry:
+            print("Spotify returned 401! Trying to reauth....")
+            if not self.fetch_access_token():
+                return None
+            return self.get_currently_playing(True)
+        elif response.status_code != 200:
+            self.err_handler(f'Failed to get currently playing song data from spotify. Code returned: {response.status_code}')
+            return None
+
+        response_data = response.json()
+        response_item = response_data['item']
+        if not response_item:
+            return None
+        cover_url = ""
+        for item in response_item['album']['images']:
+            if item['width'] == 64 and item['height'] == 64:
+                cover_url = item['url']
+
+        return CurrentlyPlayingInfo(
+            response_item['name'],
+            ', '.join(artist['name'] for artist in response_item['artists']),
+            cover_url,
+            response_item['id'],
+            response_data['progress_ms'],
+            response_item['duration_ms']
+        )
+
+class Display:
+    line1_y = 8
+    line2_y = 20
+    progress_x = 34
+    progress_y = 28
+    progress_primary_color = 0x1DB954
+    progress_secondary_color = 0x18181b
+    progress_width = 28
+
+    def __init__(self):
+        self.enabled = False
+        self.has_error = False
+        displayio.release_displays()
+        # Customize here!
+        matrix = rgbmatrix.RGBMatrix(
+            width=64, height=32, bit_depth=5,
+            rgb_pins=[board.IO1, board.IO2, board.IO3, board.IO5, board.IO4, board.IO6],
+            addr_pins=[board.IO8, board.IO7, board.IO10, board.IO9],
+            clock_pin=board.IO12, latch_pin=board.IO11, output_enable_pin=board.IO13)
+
+        self.display = framebufferio.FramebufferDisplay(matrix, auto_refresh=False)
+
+        roboto_7pt = bitmap_font.load_font("fonts/Roboto-Regular-7pt.bdf")
+        roboto_5pt = bitmap_font.load_font("fonts/Roboto-Regular-5pt.bdf")
+
+        self.line1 = adafruit_display_text.label.Label(
+            roboto_7pt,
+            color=0xf9fafb)
+        self.line1.x = self.display.width
+        self.line1.y = self.line1_y
+
+        self.line2 = adafruit_display_text.label.Label(
+            roboto_5pt,
+            color=0x3f3f46
+        )
+        self.line2.x = self.display.width
+        self.line2.y = self.line2_y
+
+        self.image_bitmap = displayio.Bitmap(32, 32, 65536)
+        image_tile_grid = displayio.TileGrid(self.image_bitmap, pixel_shader=displayio.ColorConverter(input_colorspace=displayio.Colorspace.RGB565_SWAPPED))
+
+        self.progress_bitmap = displayio.Bitmap(self.progress_width, 1, 2)
+        progress_palette = displayio.Palette(2)
+        progress_palette[0] = self.progress_primary_color
+        progress_palette[1] = self.progress_secondary_color
+        progress_tile_grid = displayio.TileGrid(self.progress_bitmap, pixel_shader = progress_palette)
+        progress_tile_grid.x = self.progress_x
+        progress_tile_grid.y = self.progress_y
+        self.progress_bar_value = 0.0
+
+        self.display_group = displayio.Group()
+        self.display_group.append(self.line1)
+        self.display_group.append(self.line2)
+        self.display_group.append(image_tile_grid)
+        self.display_group.append(progress_tile_grid)
+
+        self.jpeg_decoder = JpegDecoder()
+
+    def show_error(self, msg):
+        self.enable()
+        self.has_error = True
+        self.line1.text = "Error"
+        self.line1.color = 0xFF0000
+        self.line2.text = msg
+        self.line2.color = 0xFF0000
+        
+    def load_image_from_url(self, url):
+        gc.collect()
+        response = requests.get(url)
+        self.jpeg_decoder.open(response.content)
+        self.jpeg_decoder.decode(self.image_bitmap, 1) # Assumes the image we fetched is 64x64 and downscales it by 2^1 to 32x32
+
+    def scroll(self, line):
+        line.x = line.x - 1
+        line_width = line.bounding_box[2]
+        if line.x < -line_width + 32:
+            line.x = self.display.width
+
+    def disable(self):
+        if not self.has_error:
+            self.enabled = False
+            self.display.root_group = None
+    
+    def enable(self):
+        self.enabled = True
+        self.display.root_group = self.display_group
+
+    def update(self):
+        self.scroll(self.line1)
+        self.scroll(self.line2)
+
+        for x in range(0, int(self.progress_bar_value * self.progress_width)):
+            self.progress_bitmap[x, 0] = 0
+        for x in range(int(self.progress_bar_value * self.progress_width), self.progress_width):
+            self.progress_bitmap[x, 0] = 1
+
+        self.display.refresh(target_frames_per_second=5,minimum_frames_per_second=0)
+
+        
+display = Display()
+
+ssid = os.getenv("CIRCUITPY_WIFI_SSID")
+password = os.getenv("CIRCUITPY_WIFI_PASSWORD")
+
+spotify_refresh_token = os.getenv("SPOTIFY_REFRESH_TOKEN")
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+spotify_request_timeout = int(os.getenv("SPOTIFY_REQUEST_TIMEOUT") or "5")
+
+if spotify_refresh_token == None or spotify_client_id == None or spotify_client_secret == None:
+    display.show_error("Missing spotify credentials")
+
+pool = adafruit_connection_manager.get_radio_socketpool(wifi.radio)
+ssl_context = adafruit_connection_manager.get_radio_ssl_context(wifi.radio)
+requests = adafruit_requests.Session(pool, ssl_context)
+
+
+
+print(f"\nConnecting to {ssid}...")
+try:
+    wifi.radio.connect(ssid, password)
+except OSError as e:
+    display.show_error("No wifi connection")
+print("Connected to Wifi!")
+
+
+spotify_client = SpotifyClient(spotify_client_id, spotify_client_secret, spotify_refresh_token, display.show_error)
+
+last_update_time = 0
+last_id = 0
+
+while True:
+    if time.monotonic() > last_update_time + spotify_request_timeout and not display.has_error:
+        gc.collect()
+        currently_playing = None
+        try: 
+            currently_playing = spotify_client.get_currently_playing()
+        except MemoryError as e:
+            print("Failed to allocate memory!")
+        gc.collect()
+        if currently_playing == None:
+            display.disable()
+        else:
+            if not display.enabled:
+                display.enable()
+            display.progress_bar_value = currently_playing.progress / currently_playing.duration
+            if currently_playing.id != last_id:
+                display.load_image_from_url(currently_playing.cover_url)
+                last_id = currently_playing.id
+                display.line1.text = currently_playing.name
+                display.line2.text = currently_playing.artists
+        last_update_time = time.monotonic()
+    display.update()
